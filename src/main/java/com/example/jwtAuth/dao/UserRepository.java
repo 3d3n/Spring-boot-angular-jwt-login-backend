cd47179ea8ac2effa41a0328bcefab278e388c51
eajibade <eajibade@ecobank.com>
2019-10-30 16:41:48 +0100

    initial commit

---
+++ src/main/java/com/example/jwtAuth/dao/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.jwtAuth.dao;
+
+import com.example.jwtAuth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
